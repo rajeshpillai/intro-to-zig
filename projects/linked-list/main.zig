f0eef1dd15de32e16bac4685f6c10a929e29a485
rajeshpillai <pillai.rajesh@gmail.com>
2025-02-12 22:22:58 +0530

    LinkedList -> Single allocator

--- projects/linked-list/main.zig
+++ projects/linked-list/main.zig
@@ -2,29 +2,31 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 
 const Node = struct {
-    data: i32,
+    value: i32,
     next: ?*Node,
 
-    pub fn init(allocator: std.mem.Allocator, data: i32) !*Node {
+    pub fn init(allocator: std.mem.Allocator, value: i32) !*Node {
         const node = try allocator.create(Node);
-        node.* = Node{ .data = data, .next = null };
+        node.* = Node{ .value = value, .next = null };
         return node;
     }
 
     pub fn print(self: Node) !void {
-        try stdout.print("{d}\n", .{self.data});
+        try stdout.print("{d}\n", .{self.value});
     }
 };
 
 const LinkedList = struct {
     head: ?*Node,
+    allocator: std.mem.Allocator,
 
-    fn init() LinkedList {
-        return LinkedList{ .head = null };
+    fn init(allocator: std.mem.Allocator) LinkedList {
+        return LinkedList{ .head = null, .allocator = allocator };
     }
 
-    fn append(self: *LinkedList, allocator: std.mem.Allocator, value: i32) !void {
-        const new_node = try Node.init(allocator, value);
+    fn append(self: *LinkedList, value: i32) !void {
+        const new_node = try self.allocator.create(Node);
+        new_node.* = Node{ .value = value, .next = null };
         if (self.head == null) {
             self.head = new_node;
             return;
@@ -37,25 +39,25 @@ const LinkedList = struct {
         current.?.next = new_node;
     }
 
-    fn delete(self: *LinkedList, allocator: std.mem.Allocator, data: i32) !void {
+    fn delete(self: *LinkedList, value: i32) !void {
         if (self.head == null) return;
 
-        if (self.head.?.data == data) {
+        if (self.head.?.value == value) {
             const temp = self.head;
             self.head = self.head.?.next;
-            allocator.destroy(temp.?);
+            self.allocator.destroy(temp.?);
             return;
         }
 
         var current = self.head;
-        while (current.?.next != null and current.?.next.?.data != data) {
+        while (current.?.next != null and current.?.next.?.value != value) {
             current = current.?.next;
         }
 
         if (current.?.next != null) {
             const temp = current.?.next;
             current.?.next = current.?.next.?.next;
-            allocator.destroy(temp.?);
+            self.allocator.destroy(temp.?);
         }
     }
 
@@ -63,7 +65,7 @@ const LinkedList = struct {
         var current = self.head;
         std.debug.print("LinkedList: ", .{});
         while (current != null) {
-            std.debug.print("{} -> ", .{current.?.data});
+            std.debug.print("{} -> ", .{current.?.value});
             current = current.?.next;
         }
         std.debug.print("null\n", .{});
@@ -74,14 +76,14 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    var list = LinkedList.init();
-    try list.append(allocator, 10);
-    try list.append(allocator, 20);
-    try list.append(allocator, 30);
+    var list = LinkedList.init(allocator);
+    try list.append(10);
+    try list.append(20);
+    try list.append(30);
     std.debug.print("After appending:\n", .{});
     list.print();
 
-    try list.delete(allocator, 20);
+    try list.delete(20);
     std.debug.print("After deleting 20:\n", .{});
     list.print();
 }
